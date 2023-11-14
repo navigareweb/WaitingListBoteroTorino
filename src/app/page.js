afb8ab38b87b2b910d90d17468e210474d2d03ab
navigareweb <web@navigaresrl.com>
2023-11-14 09:21:28 +0100

    fotonuova

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./index.module.scss";
-import copertina from "../../public/600x300.png";
+import copertina from "../../public/600x300_botero.jpg";
 
 export default function Home() {
   return (
